2d159f1332f5c9b133836b7bf02dc172bb485b1a
Kartik Chitturi <kartik.prabhat.chitturi@cern.ch>
2016-12-24 17:59:39 +0000

    Set hostname properly

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+hostname $(python -c 'import sys, socket; print socket.gethostbyaddr(sys.argv[1])[0]' $(curl -s http://169.254.169.254/latest/meta-data/public-ipv4))
 /root/run-cvmfs.sh
 /root/startcondor.sh
 service gmond start
